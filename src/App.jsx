796238a7c10b325398adb74867c598f4a2441dda
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-12-25 15:10:18 +0100

    add CategoryDashboard and category management routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import BookDashboard from "./pages/book/BookDashboard.jsx";
 import SearchPage from "./pages/SearchPage";
 import AddBook from "./pages/book/AddBook.jsx";
 import UpdateBook from "./pages/book/UpdateBook.jsx";
+import CategoryDashboard from "./pages/categories/CategoryDashboard.jsx";
+import AddCategories from "./pages/categories/AddCategories.jsx";
+import UpdateCategories from "./pages/categories/UpdateCategories.jsx";
 
 function App() {
   const auth = useAuth();
@@ -54,6 +57,30 @@ function App() {
             element={
               <ProtectedRoute>
                 <UpdateBook />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/categoryDashboard"
+            element={
+              <ProtectedRoute>
+                <CategoryDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/addCategory"
+            element={
+              <ProtectedRoute>
+                <AddCategories />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/update-category/:categoryId"
+            element={
+              <ProtectedRoute>
+                <UpdateCategories />
               </ProtectedRoute>
             }
           />
